f0f546fa9c322a67c5b5f79478b9ac8998758cf9
Paul Maliakel <paulbalu93@gmail.com>
2021-07-22 22:18:55 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div class="grid-container">
+			<header class="row">
+				<div>
+					<a class="brand" href="index.html">
+						{' '}
+						Kokoland
+					</a>
+				</div>
+				<div>
+					<a href="cart.html"> Cart </a>
+					<a href="signin.html"> Signin </a>
+				</div>
+			</header>
+			<main>
+				<div class="row center">
+					{data.products.map((product) => (
+						<div class="card">
+							<a href={`/products/product._id`}>
+								<img class="medium" src={product.images.url} alt={product.name} />
+							</a>
+							<div class="card-body">
+								<a href="product.html">
+									{' '}
+									<h2> {product.name}</h2>
+								</a>
+								<div class="rating">
+									<span>
+										{' '}
+										<i class="fas fa-star"></i>
+									</span>
+									<span>
+										{' '}
+										<i class="fas fa-star"></i>
+									</span>
+									<span>
+										{' '}
+										<i class="fas fa-star"></i>
+									</span>
+									<span>
+										{' '}
+										<i class="fas fa-star"></i>
+									</span>
+									<span>
+										{' '}
+										<i class="fas fa-star"></i>
+									</span>
+								</div>
+								<div class="price"> $ {product.price}</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</main>
+			<footer class="row center">All rights reserved.</footer>
+		</div>
+	);
 }
 
 export default App;
