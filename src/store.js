388169440e6f7cc1a26f8ca96a20bdbbb995bec6
Paul Maliakel <paulbalu93@gmail.com>
2021-07-28 11:07:02 +0200

    adds redux to product screen

--- src/store.js
+++ src/store.js
@@ -2,14 +2,14 @@ import { applyMiddleware, compose, createStore, combineReducers } from 'redux';
 
 // import data from './data.js';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
 const initialState = {};
 
 // const reducer = (state, action) => {
 // 	// return { products: data.products };
 // };
-const reducer = combineReducers({ productList: productListReducer });
+const reducer = combineReducers({ productList: productListReducer, productDetails: productDetailsReducer });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
 
